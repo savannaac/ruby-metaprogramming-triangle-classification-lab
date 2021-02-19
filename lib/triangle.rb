913846abf00602210598f855830d5559020a3e03
savanna chiang <savannaac@gmail.com>
2021-02-19 00:06:01 -0600

    Metaprogramming Triangle Classification lab - complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  
+    def initialize(side1, side2, side3)
+      @side1 = side1
+      @side2 = side2
+      @side3 = side3
+
+      if side1 >= (side2 + side3) || side2 >= (side1 + side3) || side3 >= (side1 + side2)
+          raise TriangleError
+      end
+      if side1 <= 0 || side2 <= 0 || side3 <= 0
+          raise TriangleError
+      end
+      if side1 == nil || side2 == nil || side3 == nil
+          raise TriangleError
+      end
+    end
+
+    def kind
+      if @side1 == @side2 && @side1 == @side3
+          :equilateral
+      elsif @side1 == @side2 || @side2 == @side3 || @side3 == @side1
+          :isosceles
+      else
+          :scalene
+      end
+    end
+
+  class TriangleError < StandardError
+    
+    def message
+        "This is not a valid triangle."
+     end
+  end
+
 end
